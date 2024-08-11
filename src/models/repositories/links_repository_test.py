0c1c47e779a54cc06684711c89841fbf5b3a4c07
Alexandre Junior <alejunior.dev@gmail.com>
2024-08-11 18:20:16 -0300

    feat: implements link repository and tests

--- src/models/repositories/links_repository_test.py
+++ src/models/repositories/links_repository_test.py
@@ -2,32 +2,37 @@ import pytest
 import uuid
 
 from src.models.settings.db_connection_handler import db_connection_handler
-from .emails_to_invite_repository import EmailsToInviteRepository
+from .links_repository import LinksRepository
 
 db_connection_handler.connect()
 trip_id = str(uuid.uuid4())
-email_id = str(uuid.uuid4())
+link_id = str(uuid.uuid4())
 email = "johndoe@mail.com"
 
 
 @pytest.mark.skip(
     reason="Interaction with the database is not recommended in unit tests."
 )
-def test_register_email():
+def test_register_link():
     connection = db_connection_handler.get_connection()
-    emails_to_invite_repository = EmailsToInviteRepository(connection)
+    links_repository = LinksRepository(connection)
 
-    emails_to_invite_repository.register_email(
-        {"id": email_id, "trip_id": trip_id, "email": email}
+    links_repository.register_link(
+        {
+            "id": link_id,
+            "trip_id": trip_id,
+            "link": "https://www.google.com",
+            "title": "Google",
+        }
     )
 
 
 @pytest.mark.skip(
     reason="Interaction with the database is not recommended in unit tests."
 )
-def test_find_emails_from_trip():
+def test_find_links_from_trip():
     connection = db_connection_handler.get_connection()
-    emails_to_invite_repository = EmailsToInviteRepository(connection)
+    links_repository = LinksRepository(connection)
 
-    emails = emails_to_invite_repository.find_emails_from_trip(trip_id)
-    print(emails)
+    links = links_repository.find_links_from_trip(trip_id)
+    print(links)
